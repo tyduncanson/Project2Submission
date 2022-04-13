c23892048e942483e25234550994237c5b1e49d8
tyduncanson <64104410+tyduncanson@users.noreply.github.com>
2022-04-13 17:27:33 -0400

    Add files via upload

---
+++ MiniProject2/phshop/urls.py
@@ -0,0 +1,8 @@
+from itertools import product
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('products/', include('products.urls'))
+]
